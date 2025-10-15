91c07a479a849b551b983f6e40d2001d3b1e57e0
dtipgepacont <contratosdtipgepa@gmail.com>
2025-10-15 08:54:56 -0300

    Corrige navegação para SPA usando Link do react-router-dom

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Menu = () => (
   <nav style={{marginBottom: 20}}>
-    <a href="/fiscais" style={{marginRight: 10}}>Fiscais</a>
-    <a href="/empresas" style={{marginRight: 10}}>Empresas</a>
-    <a href="/contratos">Contratos</a>
+    <Link to="/fiscais" style={{marginRight: 10}}>Fiscais</Link>
+    <Link to="/empresas" style={{marginRight: 10}}>Empresas</Link>
+    <Link to="/contratos">Contratos</Link>
   </nav>
 );
 
